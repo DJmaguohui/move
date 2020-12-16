e1ea51ea7128493d6c2814c2eb392952cba49ddd
mgh <206399709@qq.com>
2020-12-16 14:12:20 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: "/",
+    component:()=>import('../views/About.vue')
+  },
+  {
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue'),
+    children: [
+      {
+        path: "index",
+        name: "首页",
+        component: () => import('../components/index/index.vue')
+      },
+      {
+        path: "course",
+        name: "课程",
+        component: () => import('../components/course/Course.vue')
+      },
+      {
+        path: "advisory",
+        name: "咨询",
+        component: () => import('../components/advisory/Advisory.vue')
+      },
+      {
+        path: "book",
+        name: "图书",
+        component: () => import('../components/book/book.vue')
+      },
+      {
+        path: "my",
+        name: "我的",
+        component: () => import('../components/my/My.vue')
+      },
+    ]
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--内容展示区域-->
+  <div>
+  <router-view></router-view>
+  </div>
+  <!--页脚-->
+    <footer>
+      <van-tabbar route>
+        <van-tabbar-item replace to="/home/index" icon="wap-home"> 首页 </van-tabbar-item>
+        <van-tabbar-item replace to="/home/course" icon="orders-o"> 课程 </van-tabbar-item>
+        <van-tabbar-item replace to="/home/advisory" icon="column"> 咨询 </van-tabbar-item>
+        <van-tabbar-item replace to="/home/book" icon="bookmark-o"> 图书 </van-tabbar-item>
+        <van-tabbar-item replace to="/home/my" icon="manager"> 我的 </van-tabbar-item>
+      </van-tabbar>
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+<style lang="scss" scoped>
+.home{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+   div{
+    flex:1;
+  }
+  footer{
+    width: 100%;
+    height: 0.5rem;
+  }
+ 
+}
+</style>
